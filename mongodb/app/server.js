15308d7ac42d304d2c4f45e5828e7fa939670296
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-10-28 21:01:30 -0400

    correct tabing

--- mongodb/app/server.js
+++ mongodb/app/server.js
@@ -14,14 +14,17 @@ var MONGO_URL = process.env.MONGO_URL + "/myproject"
 // Use connect method to connect to the server
 MongoClient.connect(MONGO_URL).then((db) => {
   const collection = db.collection("counters", {w:1});
-  collection.insert({ name: "counter", ran: 0 }, {w:1}).then((res) => {
-    return collection.findOne({ name: "counter" });
-  }).then((doc) => {
-    console.log(`Connected successfully to server ${doc.ran}`);
-    return db.close();
-  }).catch((err) => {
-    console.log(err);
-  });
+  collection.insert({ name: "counter", ran: 0 }, {w:1})
+    .then((res) => {
+      return collection.findOne({ name: "counter" });
+    })
+    .then((doc) => {
+      console.log(`Connected successfully to server ${doc.ran}`);
+      return db.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // App
@@ -29,19 +32,22 @@ const app = express();
 app.get('/', (req, res) => {
   MongoClient.connect(MONGO_URL).then((db) => {
     const collection = db.collection("counters")
-    collection.findOne({ name: "counter" }).then((doc) => {
-      console.log(`Hitting / with ${doc.ran}`);
-      res.send(`Hello world ${doc.ran}\n`);
-      return collection.update(
-        { name: "counter" },
-        { $inc: { ran: 1 }},
-        { upsert: true, w: 1}
-      );
-    }).then((doc) => {
-      return db.close();
-    }).catch((err) => {
-      console.log(err);
-    });
+    collection.findOne({ name: "counter" })
+      .then((doc) => {
+        console.log(`Hitting / with ${doc.ran}`);
+        res.send(`Hello world ${doc.ran}\n`);
+        return collection.update(
+          { name: "counter" },
+          { $inc: { ran: 1 }},
+          { upsert: true, w: 1}
+        );
+      })
+      .then((doc) => {
+        return db.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   })
 });
 
